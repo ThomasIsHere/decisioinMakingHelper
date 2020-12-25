12270ce0c787b721be9e6f82458270ca98990f2b
Thomas <lavier.thomas@gmail.com>
2020-12-25 22:36:14 +0100

    Django site creation
    Adding 2 models Question and Answer migration 0001
    Model evolution with sql migration 0002
    
    Creation 2 very basic views:
    index: to show the list of questions I'm asking myself
    detail: to show the question quand the answers to ponderatePlease enter the commit message for your changes. Lines starting

---
+++ decisionMakingHelperSite/decision/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.timezone import datetime
+
+
+class Question(models.Model):
+    question_text = models.CharField(null=True, blank=True, max_length=200)
+    creation_date = models.DateTimeField(default=datetime.now, blank=True)
+    deadline_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    sign = models.BooleanField(default=None)
+    weight = models.PositiveIntegerField(default=0)
+    grade = models.PositiveIntegerField(default=0)
+    answer_text = models.CharField(null=True, blank=True, max_length=200)
+
+    def __str__(self):
+        return self.answer_text
---
+++ decisionMakingHelperSite/decision/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-deadline_date')[:5]
+    context = {'latest_question_list': latest_question_list,}
+    return render(request, 'decision/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question,}
+    return render(request, 'decision/detail.html',context)
